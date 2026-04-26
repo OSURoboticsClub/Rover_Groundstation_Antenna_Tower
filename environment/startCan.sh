7ad8a8568724bc48036d664e981591f8353c4d2b
OSURC Rover <>
2026-04-25 17:02:44 -0700

    Added test script for bno08x

--- environment/startCan.sh
+++ environment/startCan.sh
@@ -6,3 +6,5 @@ ip link set can0 down
 ip link set can0 up type can bitrate 250000
 
 echo "CAN interface configured"
+
+chmod a+rw /dev/i2c-1
---
+++ software/util/bno08x_test.py
@@ -0,0 +1,29 @@
+import board
+import busio
+import adafruit_bno08x
+import adafruit_bno08x.i2c
+import time
+
+def main():
+	i2c = busio.I2C(board.SCL, board.SDA)
+	imu = adafruit_bno08x.i2c.BNO08X_I2C(i2c)
+
+	imu.enable_feature(adafruit_bno08x.BNO_REPORT_ROTATION_VECTOR)
+	imu.enable_feature(adafruit_bno08x.BNO_REPORT_GYROSCOPE)
+	imu.enable_feature(adafruit_bno08x.BNO_REPORT_LINEAR_ACCELERATION)
+	imu.enable_feature(adafruit_bno08x.BNO_REPORT_MAGNETOMETER)
+
+	while(True):
+		try:
+			quat = imu.quaternion
+			print(f"Orientation Quat: \n\ti: {quat[0]}, \n\tj: {quat[1]}, \n\tk: {quat[2]}, \n\tw: {quat[3]}")
+			print("Linear Acceleration: " + str(imu.linear_acceleration))
+			print("Angular Accleration: " + str(imu.gyro))
+			print("Magnetometer: " + str(imu.magnetic))
+			time.sleep(0.5)
+		except Exception as e:
+			print (f"Failed to parse packet! {type(e)}: {e}")
+
+
+if __name__ == "__main__":
+	main()
